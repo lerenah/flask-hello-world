550ed7ea754bcaea4e5e4ba0e6868cdbdebc4402
Lerena Holloway <holloway.lerena@gmail.com>
2024-07-31 21:36:09 +0100

    add db_test route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Lerena in 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://flask_piping_render_db_user:8klJtTe2LBKnf4r4M5dfNAPZP8emX7gY@dpg-cql9u3t6l47c73ani3pg-a/flask_piping_render_db")
+    conn.close()
+    return 'Database connection successful!'
+
